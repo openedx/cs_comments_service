d5148ade6aea19f8f1778042c8d8e19f3f16ef0c
Rocky Duan <dementrock@gmail.com>
2012-07-15 17:19:47 -0400

    don't need migrations

--- db/migrate/005_add_ancestry_depth_to_comments.rb
+++
@@ -1,9 +0,0 @@
-class AddAncestryDepthToComments < ActiveRecord::Migration
-  def self.up
-    add_column :comments, :ancestry_depth, :integer, :default => 0
-  end
-  
-  def self.down
-    remove_column :comments, :ancestry_depth
-  end
-end
--- db/migrate/006_add_endorsed_to_comments.rb
+++
@@ -1,9 +0,0 @@
-class AddEndorsedToComments < ActiveRecord::Migration
-  def self.up
-    add_column :comments, :endorsed, :boolean, :default => false
-  end
-  
-  def self.down
-    remove_column :comments, :endorsed
-  end
-end
