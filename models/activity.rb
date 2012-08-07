061b74aad0202f81df08878d01634fc2c23d7eaf
Rocky Duan <dementrock@gmail.com>
2012-08-07 16:27:23 -0400

    forgot to include

---
+++ models/activity.rb
@@ -0,0 +1,16 @@
+class Activity
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :anonymous, type: Boolean
+  field :activity_type, type: String
+  field :happend_at, type: Time
+
+  belongs_to :actor, class_name: "User", inverse_of: :activities, index: true, autosave: true
+  belongs_to :target, inverse_of: :activities, polymorphic: true, index: true, autosave: true
+
+  validates_presence_of :actor
+  validates_presence_of :target
+
+
+end
