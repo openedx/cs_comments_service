c5d51bbaee3cda301ee6b0ab8dde787f026c8fba
Matthew Mongeau <halogenandtoast@gmail.com>
2012-09-06 17:44:12 -0400

    Add endoresed information for threads.

--- spec/models/comment_thread_spec.rb
+++ spec/models/comment_thread_spec.rb
@@ -20,4 +20,28 @@ describe CommentThread do
     CommentThread.tag_name_valid?("_this-is-a-tag").should be_false
     CommentThread.tag_name_valid?("this-is+a-tag").should be_false
   end
+
+  context "endorsed?" do
+
+    it "knows if it is #endorsed?" do
+      thread = CommentThread.new
+      criteria = build_criteria(thread, :exists? => true)
+      thread.endorsed?.should be_true
+    end
+
+    it "knows when it is not #endorsed?" do
+      thread = CommentThread.new
+      criteria = build_criteria(thread, :exists? => false)
+      thread.endorsed?.should be_false
+    end
+
+    def build_criteria(thread, options)
+      double("criteria").tap do |criteria|
+        comments = double("relation")
+        comments.stub(:where).with(endorsed: true).and_return(criteria)
+        thread.stub(:comments).and_return(comments)
+        criteria.stub(options)
+      end
+    end
+  end
 end
