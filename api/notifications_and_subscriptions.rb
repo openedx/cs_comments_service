31368258069ff2224b5a0d08d2d5b10eb2276ce0
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-09-18 01:27:03 -0700

    Add an API call to return subscribed threads for a user.

--- api/notifications_and_subscriptions.rb
+++ api/notifications_and_subscriptions.rb
@@ -2,6 +2,10 @@ get "#{APIPREFIX}/users/:user_id/notifications" do |user_id|
   user.notifications.map(&:to_hash).to_json
 end
 
+get "#{APIPREFIX}/users/:user_id/subscribed_threads" do |user_id|
+  handle_threads_query(user.subscribed_threads)
+end
+
 post "#{APIPREFIX}/users/:user_id/subscriptions" do |user_id|
   user.subscribe(source).to_hash.to_json
 end
