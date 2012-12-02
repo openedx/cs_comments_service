ea324f7186712dfc4b196fccda4124a5f40a28d6
Your Name <kevinchugh@edx.org>
2012-12-02 15:05:12 -0500

    flagging done except returning abuse flaggers

--- api/flags.rb
+++ api/flags.rb
@@ -1,16 +1,8 @@
-put "#{APIPREFIX}/comments/:comment_id/abuse_flags" do |comment_id|
-  flag_as_abuse comment
+put "#{APIPREFIX}/threads/:thread_id/abuse_flags" do |thread_id|
+  flag_as_abuse thread
 end
 
 put "#{APIPREFIX}/threads/:thread_id/abuse_unflags" do |thread_id|
   un_flag_as_abuse thread
 end
 
-
-delete "#{APIPREFIX}/comments/:comment_id/abuse_flags" do |comment_id|
-  undo_flag_as_abuse comment
-end
-
-delete "#{APIPREFIX}/comments/:thread_id/abuse_flags" do |thread_id|
-  undo_flag_as_abuse thread
-end
