5826df36759752f74e3e2176cb49be1e507ef472
Kevin Chugh <kevinchugh@gmail.com>
2012-12-06 14:14:11 -0500

    flag comments

--- api/flags.rb
+++ api/flags.rb
@@ -6,3 +6,11 @@ put "#{APIPREFIX}/threads/:thread_id/abuse_unflags" do |thread_id|
   un_flag_as_abuse thread
 end
 
+put "#{APIPREFIX}/comments/:comment_id/abuse_flags" do |thread_id|
+  flag_as_abuse comment
+end
+
+put "#{APIPREFIX}/comments/:comment_id/abuse_unflags" do |thread_id|
+  un_flag_as_abuse comment
+end
+
