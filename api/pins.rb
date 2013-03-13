a071e152b0b9dd36cf0e49b70626d57d2c589a74
Your Name <kevinchugh@edx.org>
2013-03-13 18:59:26 -0400

    remove comments for now, b/c they're just placeholders

--- api/pins.rb
+++ api/pins.rb
@@ -6,10 +6,3 @@ put "#{APIPREFIX}/threads/:thread_id/unpin" do |thread_id|
   unpin thread
 end
 
-put "#{APIPREFIX}/comments/:comment_id/pin" do |thread_id|
-  pin comment
-end
-
-put "#{APIPREFIX}/comments/:comment_id/unpin" do |thread_id|
-  unpin comment
-end
