7ecb849db43b796890b0d202b5da4b1836812648
Your Name <kevinchugh@edx.org>
2013-03-13 18:46:20 -0400

    candidate without inline

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ APIPREFIX = CommentService::API_PREFIX
 DEFAULT_PAGE = 1
 DEFAULT_PER_PAGE = 20
 
-if RACK_ENV.to_s != "development" # disable api_key auth in test environment
+if RACK_ENV.to_s != "test" # disable api_key auth in test environment
   before do
     error 401 unless params[:api_key] == CommentService.config[:api_key]
   end
