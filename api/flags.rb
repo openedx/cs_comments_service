8a4f73f80b0ef8b5f41dfc50da0dd1be655acbdf
Your Name <kevinchugh@edx.org>
2013-04-23 13:25:57 -0400

    refactor plural verbs

--- api/flags.rb
+++ api/flags.rb
@@ -1,15 +1,15 @@
-put "#{APIPREFIX}/threads/:thread_id/abuse_flags" do |thread_id|
+put "#{APIPREFIX}/threads/:thread_id/abuse_flag" do |thread_id|
   flag_as_abuse thread
 end
 
-put "#{APIPREFIX}/threads/:thread_id/abuse_unflags" do |thread_id|
+put "#{APIPREFIX}/threads/:thread_id/abuse_unflag" do |thread_id|
   un_flag_as_abuse thread
 end
 
-put "#{APIPREFIX}/comments/:comment_id/abuse_flags" do |comment_id|
+put "#{APIPREFIX}/comments/:comment_id/abuse_flag" do |comment_id|
   flag_as_abuse comment
 end
 
-put "#{APIPREFIX}/comments/:comment_id/abuse_unflags" do |comment_id|
+put "#{APIPREFIX}/comments/:comment_id/abuse_unflag" do |comment_id|
   un_flag_as_abuse comment
 end
