d242d39c7ed166b4d6f2c1ba1ee78f6240bf070e
Your Name <kevinchugh@edx.org>
2013-04-23 11:25:24 -0400

    update object identifier in API cal

--- api/flags.rb
+++ api/flags.rb
@@ -6,10 +6,10 @@ put "#{APIPREFIX}/threads/:thread_id/abuse_unflags" do |thread_id|
   un_flag_as_abuse thread
 end
 
-put "#{APIPREFIX}/comments/:comment_id/abuse_flags" do |thread_id|
+put "#{APIPREFIX}/comments/:comment_id/abuse_flags" do |comment_id|
   flag_as_abuse comment
 end
 
-put "#{APIPREFIX}/comments/:comment_id/abuse_unflags" do |thread_id|
+put "#{APIPREFIX}/comments/:comment_id/abuse_unflags" do |comment_id|
   un_flag_as_abuse comment
 end
