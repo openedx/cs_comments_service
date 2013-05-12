44dbec1d4dda47dc1d949d854e715abe78c584f1
Arjun Singh <arjun810@gmail.com>
2013-05-12 04:08:42 -0700

    Speed up the active_threads page for a single user by about 1.5-2x by bulking some queries.

--- api/users.rb
+++ api/users.rb
@@ -30,9 +30,14 @@ get "#{APIPREFIX}/users/:user_id/active_threads" do |user_id|
   page = [num_pages, [1, page].max].min
 
   paged_active_contents = active_contents.page(page).per(per_page)
-  paged_active_threads = paged_active_contents.map(&get_thread_id)
-                                              .uniq.map(&get_thread)
-  collection = paged_active_threads.map{|t| t.to_hash(recursive: true)}
+  paged_thread_ids = paged_active_contents.map(&get_thread_id).uniq
+  paged_active_threads = CommentThread.find(paged_thread_ids)
+
+  # Fetch all the usernames in bulk to save on queries. Since we're using the
+  # identity map, the users won't need to be fetched again later.
+  User.only(:username).find(paged_active_threads.map{|x| x.author_id})
+
+  collection = paged_active_threads.map{|t| t.to_hash recursive: true}
   collection = author_contents_only(collection, user_id)
 
   {
