67a6d6f72a76693fc6be31ab472209ccc403dcfa
jimabramson <jsa@edx.org>
2013-12-09 21:32:15 -0500

    rewrite TERM to QUIT for more graceful shutdowns

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,16 @@ worker_processes 4
 timeout 25
 preload_app true
 
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+end
+
 after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Waiting for master to send QUIT'
+  end
   ::Mongoid.default_session.disconnect
 end
