d116df781889a52048b99097d6887fe50e322a09
Carson Gee <x@carsongee.com>
2014-03-10 15:28:38 -0400

    Removing TERM to QUIT translator

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,17 +5,3 @@ preload_app true
 data_dir = ENV['DATA_DIR'] || Dir.tmpdir
 listen "unix:#{data_dir}/forum.sock", :backlog => 512
 pid "#{data_dir}/forum_unicorn.pid"
-
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Waiting for master to send QUIT'
-  end
-  ::Mongoid.default_session.disconnect
-end
--- config/unicorn_tcp.rb
+++ config/unicorn_tcp.rb
@@ -10,16 +10,3 @@ listen "#{listen_host}:#{listen_port}", :tcp_nopush => true
 data_dir = ENV['DATA_DIR'] || Dir.tmpdir
 pid "#{data_dir}/forum_unicorn.pid"
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Waiting for master to send QUIT'
-  end
-  ::Mongoid.default_session.disconnect
-end
