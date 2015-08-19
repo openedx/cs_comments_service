265ceeab32950718fd12744e4b266c3ec6ef7831
Brian Beggs <bbeggs@edx.org>
2015-08-19 11:21:31 -0400

    Updated context revert migration to use bulk operations

--- scripts/db/revert-migrate-008-context.js
+++ scripts/db/revert-migrate-008-context.js
@@ -3,9 +3,7 @@ db.contents.dropIndex({ _type: 1, course_id: 1, context: 1, pinned: -1, created_
 db.contents.dropIndex({ _type: 1, commentable_id: 1, context: 1, pinned: -1, created_at: -1 })
 
 print ("Removing context from all comment threads\n");
-db.contents.update(
-    {_type: "CommentThread"},
-    {$unset: {context: ""}},
-    {multi: true}
-);
+var bulk = db.contents.initializeUnorderedBulkOp();
+bulk.find( {_type: "CommentThread", context: {$exists: true}} ).update(  {$unset: {context: ""}} );
+bulk.execute();
 printjson (db.runCommand({ getLastError: 1, w: "majority", wtimeout: 5000 } ));
