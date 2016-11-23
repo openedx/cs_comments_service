a8afdb70bc3b58eaa63e3dd816f78d0938a4609a
Feanil Patel <feanil@edx.org>
2016-11-23 05:47:18 -0500

    Test.

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -5,6 +5,6 @@ export MONGOHQ_URL="mongodb://mongo.edx:27017/cs_comments_service_test"
 
 cd /edx/app/forum/cs_comments_service
 
-gem update bundler # Ensure we use the latest version of bundler. Travis' default version of outdated.
+#gem update bundler # Ensure we use the latest version of bundler. Travis' default version of outdated.
 
 bundle exec rspec
