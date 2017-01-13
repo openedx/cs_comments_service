8d373a0a4b7191cda3737e5f51539ba61ce9478e
Robert Raposa <rraposa@edx.org>
2017-01-13 14:49:29 -0500

    Update rake tests.

--- spec/lib/tasks/search_rake_spec.rb
+++ spec/lib/tasks/search_rake_spec.rb
@@ -3,47 +3,63 @@ require 'elasticsearch'
 
 describe "search:rebuild_index" do
   include_context "rake"
-  include_context 'search_enabled'
+
+  before do
+    TaskHelpers::ElasticsearchHelper.stub!(:rebuild_index)
+  end
 
   its(:prerequisites) { should include("environment") }
 
-  def create_thread_and_delete_index()
-    thread = create(:comment_thread, body: 'the best test body', course_id: 'test_course_id')
-    refresh_es_index
-    TaskHelpers::ElasticsearchHelper.delete_index(Content::ES_INDEX_NAME)
-  end
-
-  # Returns newest created index by comparing current against array of indices_before.
-  # Has side effects of asserting there is 1 new index and refreshing this newest index.
-  def get_newest_index_name(indices_before)
-    indices_after = Elasticsearch::Model.client.indices.get_aliases.keys
-    new_indices = indices_after - indices_before
-    new_indices.length.should eq 1
-    index_name = new_indices[0]
-    refresh_es_index(index_name)
-    index_name
-  end
-
-  it "builds new index without switching alias" do
-    create_thread_and_delete_index
-    indices_before = Elasticsearch::Model.client.indices.get_aliases.keys
+  it "calls rebuild_index with defaults" do
+    TaskHelpers::ElasticsearchHelper.should_receive(:rebuild_index).with(nil, 500, 0)
 
     subject.invoke
-
-    index_name = get_newest_index_name(indices_before)
-    index_name.should_not eq Content::ES_INDEX_NAME
-    Elasticsearch::Model.client.search(index: index_name)['hits']['total'].should be > 0
   end
 
-  it "builds new index and points alias to it, first deleting index with same name as alias" do
-    create_thread_and_delete_index
-    TaskHelpers::ElasticsearchHelper.create_index(Content::ES_INDEX_NAME)
-    call_move_alias = true
+  it "calls rebuild_index with arguments" do
+    # Rake calls receive arguments as strings.
+    call_move_alias = 'true'
+    batch_size = '100'
+    sleep_time = '2'
+    TaskHelpers::ElasticsearchHelper.should_receive(:rebuild_index).with(
+          Content::ES_INDEX_NAME, batch_size.to_i, sleep_time.to_i
+    )
 
-    subject.invoke(call_move_alias)
-    refresh_es_index
+    subject.invoke(call_move_alias, batch_size, sleep_time)
+  end
+end
 
-    Elasticsearch::Model.client.search(index: Content::ES_INDEX_NAME)['hits']['total'].should be > 0
+describe "search:catchup" do
+  include_context "rake"
+
+  before do
+    TaskHelpers::ElasticsearchHelper.stub!(:catchup_index)
   end
 
+  its(:prerequisites) { should include("environment") }
+
+  it "calls catchup with defaults" do
+    TaskHelpers::ElasticsearchHelper.should_receive(:catchup_index).with(
+        anything, Content::ES_INDEX_NAME, 500, 0
+    ) do |start_time_arg|
+      start_time_arg.should be_within(1.second).of Time.now
+    end
+
+    subject.invoke
+  end
+
+  it "calls catchup with arguments" do
+    # Rake calls receive arguments as strings.
+    minutes = '2'
+    index_name = 'some_index'
+    batch_size = '100'
+    sleep_time = '2'
+    TaskHelpers::ElasticsearchHelper.should_receive(:catchup_index).with(
+        anything, index_name, batch_size.to_i, sleep_time.to_i
+    ) do |start_time_arg|
+      start_time_arg.should be_within((60 * minutes.to_i + 1).second).of Time.now
+    end
+
+    subject.invoke(minutes, index_name, batch_size, sleep_time)
+  end
 end
