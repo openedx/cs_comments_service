53c7b23d0f29f5674edb02533b8fa55922a9bffb
Kshitij Sobti <kshitij@sobti.in>
2022-09-14 11:48:41 +0530

    feat: Update testing system

--- .github/run_tests.sh
+++ .github/run_tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -xe
 . /edx/app/forum/forum_env
-export MONGOHQ_URL="mongodb://mongo.edx:27017/cs_comments_service_test"
+export MONGOHQ_URL="mongodb://mongodb:27017/cs_comments_service_test"
 
 cd /edx/app/forum/cs_comments_service
 
 bundle install
 
 # allow dependent services to finish start up (e.g. ElasticSearch, Mongo)
-sleep 10
+# sleep 10
 
 # Use 'bin/rspec -fd' to print test names for debugging
 # Printing test names can be especially helpful for tracking down test
 # failure differences between CI and Mac, because tests are loaded
 # and run in different orders.
-bin/rspec
+bin/rspec -fd
